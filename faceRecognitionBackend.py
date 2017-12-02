4b36adf2effd8cdcaa9f86a4240c94bb6e9e2e4f
Johan Cornelissen <johan1252@gmail.com>
2017-12-02 12:37:41 -0500

    Adding test photos

--- faceRecognitionBackend.py
+++ faceRecognitionBackend.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Copyright 2016 Google Inc. All Rights Reserved.
 #
@@ -13,18 +13,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# [START vision_quickstart]
+import io
+import os
+from PIL import Image, ImageDraw
 
+# Imports the Google Cloud client library
+# [START migration_import]
+from google.cloud import vision
+from google.cloud.vision import types
 
 def run_quickstart():
-    # [START vision_quickstart]
-    import io
-    import os
-
-    # Imports the Google Cloud client library
-    # [START migration_import]
-    from google.cloud import vision
-    from google.cloud.vision import types
-    # [END migration_import]
 
     # Instantiates a client
     # [START migration_client]
@@ -34,7 +33,7 @@ def run_quickstart():
     # The name of the image file to annotate
     file_name = os.path.join(
         os.path.dirname(__file__),
-        'resources/wakeupcat.jpg')
+        'testPhoto.jpg')
 
     # Loads the image into memory
     with io.open(file_name, 'rb') as image_file:
@@ -51,6 +50,60 @@ def run_quickstart():
         print(label.description)
     # [END vision_quickstart]
 
+def detect_face(face_file, max_results=4):
+    """Uses the Vision API to detect faces in the given file.
+
+    Args:
+        face_file: A file-like object containing an image with faces.
+
+    Returns:
+        An array of Face objects with information about the picture.
+    """
+    client = vision.ImageAnnotatorClient()
+
+    content = face_file.read()
+    image = types.Image(content=content)
+    
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    print('Labels:')
+    for label in labels:
+        print(label.description)
+
+    return client.face_detection(image=image).face_annotations
+
+def highlight_faces(image, faces, output_filename):
+    """Draws a polygon around the faces, then saves to output_filename.
+
+    Args:
+      image: a file containing the image with the faces.
+      faces: a list of faces found in the file. This should be in the format
+          returned by the Vision API.
+      output_filename: the name of the image file to be created, where the
+          faces have polygons drawn around them.
+    """
+    im = Image.open(image)
+    draw = ImageDraw.Draw(im)
+
+    for face in faces:
+        box = [(vertex.x, vertex.y)
+               for vertex in face.bounding_poly.vertices]
+        draw.line(box + [box[0]], width=5, fill='#00ff00')
+
+    im.save(output_filename)
+    
+def main(input_filename, output_filename, max_results):
+    with open(input_filename, 'rb') as image:
+        faces = detect_face(image, max_results)
+        print('Found {} face{}'.format(
+            len(faces), '' if len(faces) == 1 else 's'))
+
+        print('Writing to file {}'.format(output_filename))
+        # Reset the file pointer, so we can read the file again
+        image.seek(0)
+        highlight_faces(image, faces, output_filename)
 
 if __name__ == '__main__':
-    run_quickstart()
+    main('testPhotoParv.jpg','testParvOutput.jpg',5)
